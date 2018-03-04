2d96c98fad1dd522ba166a9db8a36040bdea2a20
justinbeharry <jsbeharr@umich.edu>
2018-03-04 00:40:51 -0500

    Replace me fully functional
    *Replaced files in folder with those in home
    *Moves old file into a new directory

--- replace.sh
+++ replace.sh
@@ -5,4 +5,17 @@
 # Any old dotfile with be marked as old_<file> and placed
 # in a new folder called old_dotfiles
 
+FILES=$(ls ~/dotfiles | egrep -v "brew_installation.sh|collect.sh|README.md|iterm2_profile|replace.sh")
 
+mkdir ~/old_dotfiles
+
+for file in $FILES
+do
+  if [ -f ~/.$file ];then
+    mv ~/.$file ~/old_$file
+    mv ~/old_$file ~/old_dotfiles
+    echo "moved old .$file into old_dotfiles/"
+  fi
+  cp $file ~/.$file
+  echo "Copied $file into home"
+done
