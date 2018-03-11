183443018c3988b92f849f06938b06aeadd935f9
justinbeharry <jsbeharr@umich.edu>
2018-03-11 17:30:13 -0400

    - plugins in hyper and + key binding in tmux

--- hyper.js
+++ hyper.js
@@ -119,7 +119,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperborder', 'hyperminimal', 'gitrocket', 'space-pull'],
+  plugins: ['hyperborder', 'hyperminimal'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
